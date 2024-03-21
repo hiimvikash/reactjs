b5cf3aa63b32f75757298a807106ed4158af954a
Vikash Gupta <heyimvikash@gmail.com>
2024-03-21 13:28:10 +0530

    added mutation

--- react16-rrtkQuery/redux/apiSlice.js
+++ react16-rrtkQuery/redux/apiSlice.js
@@ -5,9 +5,17 @@ export const apiSlice = createApi({
   reducerPath: 'api', 
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
   endpoints: (builder) => ({
-    fetchUsers: builder.query({query: () => 'users'})
+    fetchUsers: builder.query({query: () => 'users', providesTags : ['getUser']}),
+    createUser : builder.mutation({query : (userData)=> 
+        ({
+            url : "users",
+            method : 'POST',
+            body : userData
+        }),
+        invalidatesTags : ['getUser']
+    })
   }),
 });
 
 // Export generated hooks for each endpoint
-export const { useFetchUsersQuery } = apiSlice;
+export const { useFetchUsersQuery, useCreateUserMutation } = apiSlice;
